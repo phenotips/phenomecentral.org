9e07578717b0c8ae448e3fc2cc9b4a5845f89dab
Sergiu Dumitriu <sergiu@phenotips.org>
2019-04-02 14:19:23 -0400

    PC-325: Integration Tests Replaced instance pseudo-constants with proper constants

--- tests/src/test/java/org/phenotips/endtoendtests/testcases/AddingUsersTests.java
+++ tests/src/test/java/org/phenotips/endtoendtests/testcases/AddingUsersTests.java
@@ -35,31 +35,33 @@ import org.testng.annotations.Test;
  */
 public class AddingUsersTests extends BaseTest
 {
-    final HomePage aHomePage = new HomePage(theDriver);
-
-    final UserSignUpPage aUserSignUpPage = new UserSignUpPage(theDriver);
-
     // Strings of messages to verify:
 
-    final String confirmationMessageCheck =
+    private static final String CONFIRMATION_MESSAGE_CHECK =
         "Thank you for your interest in PhenomeCentral. We took note of your request and we will process it shortly.";
 
-    final String pendingApprovalMessageCheck = "Please wait for your account to be approved. Thank you.";
+    private static final String PENDING_APPROVAL_MESSAGE_CHECK =
+        "Please wait for your account to be approved. Thank you.";
 
-    final String randomChars = RandomStringUtils.randomAlphanumeric(5);
+    private static final String RANDOM_CHARS = RandomStringUtils.randomAlphanumeric(5);
 
     // Common Strings for creation of patients:
 
-    final String password = "123456";
+    private static final String PASSWORD = "123456";
 
-    final String affiliation = "someaffilation";
+    private static final String AFFILIATION = "someaffilation";
 
-    final String referrer = "some referrer";
+    private static final String REFERRER = "some referrer";
 
-    final String justification = "some reason";
+    private static final String JUSTIFICATION = "some reason";
 
-    List<String> loSectionTitlesCheck = new ArrayList<>(Arrays.asList("MY MATCHES", "MY PATIENTS\n ",
-        "PATIENTS SHARED WITH ME\n ", "MY GROUPS\n ", "PUBLIC DATA\n "));
+    private static final List<String> loSectionTitlesCheck =
+        new ArrayList<>(Arrays.asList("MY MATCHES", "MY PATIENTS\n ",
+            "PATIENTS SHARED WITH ME\n ", "MY GROUPS\n ", "PUBLIC DATA\n "));
+
+    private final HomePage aHomePage = new HomePage(theDriver);
+
+    private final UserSignUpPage aUserSignUpPage = new UserSignUpPage(theDriver);
 
     // Adds a user through the admin's Users page. Approve the user and login and ensure that dashboard widgets (i.e.
     // My Patients, My Families, etc. are visible.
@@ -68,17 +70,16 @@ public class AddingUsersTests extends BaseTest
     {
         this.aHomePage.navigateToLoginPage().loginAsAdmin()
             .navigateToAdminSettingsPage().navigateToAdminUsersPage()
-            .addUser("AutoAdded1" + this.randomChars, "AutoAdded1Last", this.password,
-                "AutoAdded1" + this.randomChars + "@somethingsomething.cjasdfj", this.affiliation, this.referrer,
-                this.justification)
+            .addUser("AutoAdded1" + RANDOM_CHARS, "AutoAdded1Last", PASSWORD,
+                "AutoAdded1" + RANDOM_CHARS + "@somethingsomething.cjasdfj", AFFILIATION, REFERRER, JUSTIFICATION)
             .navigateToPendingUsersPage()
             .approveNthPendingUser(1);
 
         this.aHomePage.logOut()
-            .loginAs("AutoAdded1" + this.randomChars + "AutoAdded1Last", this.password)
+            .loginAs("AutoAdded1" + RANDOM_CHARS + "AutoAdded1Last", PASSWORD)
             .navigateToHomePage();
 
-        Assert.assertEquals(this.aHomePage.getSectionTitles(), this.loSectionTitlesCheck);
+        Assert.assertEquals(this.aHomePage.getSectionTitles(), loSectionTitlesCheck);
 
         this.aHomePage.logOut();
     }
@@ -87,25 +88,24 @@ public class AddingUsersTests extends BaseTest
     @Test
     public void userSignUpNotApproved()
     {
-        final String firstname = "PublicSignUp" + this.randomChars;
-        final String lastname = "Auto" + this.randomChars;
+        final String firstname = "PublicSignUp" + RANDOM_CHARS;
+        final String lastname = "Auto" + RANDOM_CHARS;
 
         this.aHomePage.navigateToSignUpPage()
-            .requestAccount(firstname, lastname, this.password, this.password,
-                firstname + "@akjsjdf.cjsjdfn", this.affiliation, this.referrer, this.justification);
+            .requestAccount(firstname, lastname, PASSWORD, PASSWORD, firstname + "@akjsjdf.cjsjdfn", AFFILIATION,
+                REFERRER, JUSTIFICATION);
 
-        Assert.assertEquals(this.aUserSignUpPage.getConfirmationMessage(), this.confirmationMessageCheck);
+        Assert.assertEquals(this.aUserSignUpPage.getConfirmationMessage(), CONFIRMATION_MESSAGE_CHECK);
         System.out.println("Request Recieved Msg: " + this.aUserSignUpPage.getConfirmationMessage());
 
-        this.aHomePage.navigateToLoginPage()
-            .loginAs(firstname + lastname, this.password);
+        this.aHomePage.navigateToLoginPage().loginAs(firstname + lastname, PASSWORD);
 
         System.out.println("Approval Pending Msg: " + this.aHomePage.getApprovalPendingMessage());
-        Assert.assertEquals(this.aHomePage.getApprovalPendingMessage(), this.pendingApprovalMessageCheck);
+        Assert.assertEquals(this.aHomePage.getApprovalPendingMessage(), PENDING_APPROVAL_MESSAGE_CHECK);
         Assert.assertEquals(this.aHomePage.navigateToAllPatientsPage().getApprovalPendingMessage(),
-            this.pendingApprovalMessageCheck);
+            PENDING_APPROVAL_MESSAGE_CHECK);
         Assert.assertEquals(this.aHomePage.navigateToCreateANewPatientPage().getApprovalPendingMessage(),
-            this.pendingApprovalMessageCheck);
+            PENDING_APPROVAL_MESSAGE_CHECK);
 
         this.aHomePage.logOut();
     }
@@ -116,21 +116,19 @@ public class AddingUsersTests extends BaseTest
     @Test
     public void userSignUpApproved()
     {
-        String firstName = "PublicSignUpApproved" + this.randomChars;
-        String lastName = "Auto" + this.randomChars;
+        String firstName = "PublicSignUpApproved" + RANDOM_CHARS;
+        String lastName = "Auto" + RANDOM_CHARS;
         String username = firstName + lastName;
 
-        this.aHomePage.navigateToSignUpPage()
-            .requestAccount(firstName, lastName, this.password, this.password,
-                firstName + "@akjsjdf.cjsjdfn", this.affiliation, this.referrer, this.justification);
+        this.aHomePage.navigateToSignUpPage().requestAccount(firstName, lastName, PASSWORD, PASSWORD,
+            firstName + "@akjsjdf.cjsjdfn", AFFILIATION, REFERRER, JUSTIFICATION);
 
-        Assert.assertEquals(this.aUserSignUpPage.getConfirmationMessage(), this.confirmationMessageCheck);
+        Assert.assertEquals(this.aUserSignUpPage.getConfirmationMessage(), CONFIRMATION_MESSAGE_CHECK);
 
-        this.aHomePage.navigateToLoginPage()
-            .loginAs(username, this.password);
+        this.aHomePage.navigateToLoginPage().loginAs(username, PASSWORD);
 
         System.out.println("Approval Pending Msg: " + this.aHomePage.getApprovalPendingMessage());
-        Assert.assertEquals(this.aHomePage.getApprovalPendingMessage(), this.pendingApprovalMessageCheck);
+        Assert.assertEquals(this.aHomePage.getApprovalPendingMessage(), PENDING_APPROVAL_MESSAGE_CHECK);
 
         this.aHomePage.logOut();
         this.aHomePage.navigateToLoginPage()
@@ -139,54 +137,58 @@ public class AddingUsersTests extends BaseTest
             .navigateToPendingUsersPage()
             .approvePendingUser(username)
             .logOut()
-            .loginAs(username, this.password)
+            .loginAs(username, PASSWORD)
             .navigateToHomePage();
 
         System.out.println("Titles Found: " + this.aHomePage.getSectionTitles());
-        Assert.assertEquals(this.aHomePage.getSectionTitles(), this.loSectionTitlesCheck);
+        Assert.assertEquals(this.aHomePage.getSectionTitles(), loSectionTitlesCheck);
     }
 
-    // Error check the input fields, ensures that a request is not sent if any one of the required fields are missing or invalid.
-    // This test keeps trying to create the user on the same request form, so it will fail if there was no error check and we
+    // Error check the input fields, ensures that a request is not sent if any one of the required fields are missing or
+    // invalid.
+    // This test keeps trying to create the user on the same request form, so it will fail if there was no error check
+    // and we
     // navigate away from that page.
     @Test
     public void errorCheckFields()
     {
         this.aHomePage.navigateToSignUpPage()
-            .requestAccount("PublicDupe" + this.randomChars, "Duped" + this.randomChars, "123456", "123456",
-                "PublicSignUpDupe" + this.randomChars + "@something.something", "none",
+            .requestAccount("PublicDupe" + RANDOM_CHARS, "Duped" + RANDOM_CHARS,
+                "123456", "123456",
+                "PublicSignUpDupe" + RANDOM_CHARS + "@something.something", "none",
                 "Test server", "Some reason");
 
         // Duplicate Username
         this.aHomePage.navigateToHomePage()
             .navigateToSignUpPage()
-            .requestAccount("PublicDupe" + this.randomChars, "Duped" + this.randomChars, "123456", "123456",
-                "PublicSignUpDupe" + this.randomChars + "@something.something", "none",
+            .requestAccount("PublicDupe" + RANDOM_CHARS, "Duped" + RANDOM_CHARS,
+                "123456", "123456",
+                "PublicSignUpDupe" + RANDOM_CHARS + "@something.something", "none",
                 "Test server", "Some reason")
 
             // Mismatching passwords
-            .requestAccount("MisMatched PassWord" + this.randomChars, "Mismatch", "123456", "123456 ",
-                "PublicSignUpDupe" + this.randomChars + "@something.something", "none",
+            .requestAccount("MisMatched PassWord" + RANDOM_CHARS, "Mismatch", "123456", "123456 ",
+                "PublicSignUpDupe" + RANDOM_CHARS + "@something.something", "none",
                 "Test server", "Some reason")
 
             // Invalid Email
-            .requestAccount("Invalid Email" + this.randomChars, "Invalidated", "123456", "123456",
-                "PublicSignUpDupe" + this.randomChars + "@NoExtension", "none",
+            .requestAccount("Invalid Email" + RANDOM_CHARS, "Invalidated", "123456", "123456",
+                "PublicSignUpDupe" + RANDOM_CHARS + "@NoExtension", "none",
                 "Test server", "Some reason")
 
             // Lacking first name
             .requestAccount("", "First Name Missing", "123456", "123456",
-                "PublicSignUpDupe" + this.randomChars + "@something.something", "none",
+                "PublicSignUpDupe" + RANDOM_CHARS + "@something.something", "none",
                 "Test server", "Some reason")
 
             // Lacking last name
             .requestAccount("LastName Missing", "", "123456", "123456",
-                "PublicSignUpDupe" + this.randomChars + "@something.something", "none",
+                "PublicSignUpDupe" + RANDOM_CHARS + "@something.something", "none",
                 "Test server", "Some reason")
 
             // Lacking password
-            .requestAccount("MisMatched PassWord" + this.randomChars, "Mismatch", "", "",
-                "PublicSignUpDupe" + this.randomChars + "@something.something", "none",
+            .requestAccount("MisMatched PassWord" + RANDOM_CHARS, "Mismatch", "", "",
+                "PublicSignUpDupe" + RANDOM_CHARS + "@something.something", "none",
                 "Test server", "Some reason")
 
             // Lacking email
@@ -195,8 +197,8 @@ public class AddingUsersTests extends BaseTest
                 "Test server", "Some reason")
 
             // Lack of Affiliation
-            .requestAccount("No Affiliation" + this.randomChars, "Reasoning", "123456", "123456",
-                "PublicSignUpDupe" + this.randomChars + "@something.something", "",
+            .requestAccount("No Affiliation" + RANDOM_CHARS, "Reasoning", "123456", "123456",
+                "PublicSignUpDupe" + RANDOM_CHARS + "@something.something", "",
                 "Test server", "Some reason")
             .cancelRequestingAccount()
             .navigateToLoginPage();
